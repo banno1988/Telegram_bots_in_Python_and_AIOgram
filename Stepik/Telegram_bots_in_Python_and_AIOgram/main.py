5f70b96a1686799f3397bb186b58ef553a2ed087
Ivan Grinin <banno@mail.ru>
2023-04-16 23:23:44 +0300

    Отправляет сообщение

--- Stepik/Telegram_bots_in_Python_and_AIOgram/main.py
+++ Stepik/Telegram_bots_in_Python_and_AIOgram/main.py
@@ -1,11 +1,28 @@
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import requests
+import time
 
 
+API_URL: str = 'https://api.telegram.org/bot'
+BOT_TOKEN: str = '6042745819:AAFmNd8jhEx3-mkHAyYz5dbTnkhxAyecuoQ'
+TEXT: str = 'Ура! Классный апдейт!'
+MAX_COUNTER: int = 100
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+offset: int = -2
+counter: int = 0
+chat_id: int
 
 
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
